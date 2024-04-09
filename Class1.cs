1663a7510158748f36b39b19ef8046eb5da6d371
zhouxiang <2488632742@qq.com>
2024-04-09 22:53:28 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Xml.Linq;
+using System.Threading;
+using Org.BouncyCastle.Utilities;
+
+
+namespace Mynamespace
+{
+    public static class SerialCom
+    {
+        public static SerialPort com { get; set; } = new SerialPort();
+
+        public static string? com_name { get; set; }
+        // 波特率
+
+        public static int com_Bound { get; set; }
+        //数据位
+
+        public static int com_DataBit { get; set; }
+        // 校验位
+
+        public static string? com_Verify { get; set; }
+        // 停止位
+
+        public static string? com_StopBit { get; set; }
+        // 串口的打开状态标记位
+
+        public static bool OpenState { get; set; }
+        //数据显示
+        public static List<string> comdata = new List<string>();
+    }
+    public class Mycom
+    {
+        public void ComOpen()
+        {
+            if (SerialCom.OpenState == false)
+            {
+                SerialCom.com.PortName = SerialCom.com_name;
+                SerialCom.com.BaudRate = SerialCom.com_Bound;
+                SerialCom.com.DataBits = SerialCom.com_DataBit;
+                if (SerialCom.com_StopBit == "1") SerialCom.com.StopBits = System.IO.Ports.StopBits.One;
+                if (SerialCom.com_StopBit == "2") SerialCom.com.StopBits = System.IO.Ports.StopBits.Two;
+
+                if (SerialCom.com_Verify == "None") SerialCom.com.Parity = System.IO.Ports.Parity.None;
+                if (SerialCom.com_Verify == "odd") SerialCom.com.Parity = System.IO.Ports.Parity.Odd;
+                if (SerialCom.com_Verify == "Even") SerialCom.com.Parity = System.IO.Ports.Parity.Even;
+                SerialCom.com.NewLine = "\r\n";
+                comthread();
+
+            }
+            else
+            {
+                // 关闭串口
+                SerialCom.comdata.Add("关闭串口");
+                SerialCom.OpenState = false;
+                SerialCom.com.Close();
+            }
+        }
+
+
+        private void ReadDada()
+        {
+            SerialCom.comdata.Add("打开串口完成");
+
+            SerialCom.OpenState = true;
+            while (SerialCom.OpenState)
+            {
+                Thread.Sleep(50);
+                try
+                {
+                    //查询串口中目前保存了多少数据
+                    int n = SerialCom.com.BytesToRead;
+                    byte[] buf = new byte[n];
+                    SerialCom.com.Read(buf, 0, n);
+                    if (buf.Length > 0)
+                    {
+                        string str = Encoding.Default.GetString(buf);
+                        SerialCom.comdata.Add(str);
+                    }
+                }
+                catch
+                {
+                    SerialCom.OpenState = false;
+                    SerialCom.com.Close();
+
+                }
+            }
+        }
+        private void comthread()
+        {
+            SerialCom.com.Open();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            SerialCom.com.Encoding = Encoding.GetEncoding("GB2312");
+            Thread thread = new Thread(ReadDada);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+
+        public void WriteData(byte[] bytes)
+        {
+            try
+            {
+                if (SerialCom.OpenState && bytes != null)
+                {
+                    SerialCom.com.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+
+
+
+
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Windows.Threading;
+using Mynamespace;
+using System.Text.Encodings;
+using Org.BouncyCastle.Ocsp;
+using System.Data;
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dtimer = new DispatcherTimer();
+        private Mycom mCom = new Mycom();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (SerialCom.OpenState == true)
+                Revdata.Text = string.Join("\n", SerialCom.comdata);
+
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && Senddata.IsFocused)
+            {
+                // 阻止Enter键在TextBox中产生默认的换行行为  
+                e.Handled = true;
+
+                // 调用按钮的点击事件  
+                mCom.WriteData(Encoding.UTF8.GetBytes(Senddata.Text));
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dtimer.Interval = TimeSpan.FromMilliseconds(100);
+            dtimer.Tick += new EventHandler(timer_Tick);
+            dtimer.Start();
+            string[] ports = SerialPort.GetPortNames();
+            this.PortName.ItemsSource = ports;
+            this.PortName.SelectedIndex = 0;
+
+            string[] baudrate = new string[] { "300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "43000", "56000", "57600", "115200" };
+            this.Buadrate.ItemsSource = baudrate;
+            this.Buadrate.SelectedIndex = 0;
+
+            this.Databit.Items.Add(8);
+            this.Databit.Items.Add(7);
+            this.Databit.SelectedIndex = 0;
+
+            this.Stopbit.Items.Add(2);
+            this.Stopbit.Items.Add(1);
+            this.Stopbit.SelectedIndex = 0;
+
+            this.Parity.Items.Add("None");
+            this.Parity.Items.Add("Even");
+            this.Parity.Items.Add("Odd");
+            this.Parity.SelectedIndex = 0;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            this.PortName.ItemsSource = ports;
+            this.PortName.SelectedIndex = 0;
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            mCom.WriteData(Encoding.UTF8.GetBytes(Senddata.Text));
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            SerialCom.com_name = PortName.Text;
+            SerialCom.com_Bound = int.Parse(Buadrate.Text);
+            SerialCom.com_DataBit = int.Parse(Databit.Text);
+            SerialCom.com_StopBit = Stopbit.Text;
+            SerialCom.com_Verify = Parity.Text;
+            mCom.ComOpen();
+        }
+
+    }
+}
+
